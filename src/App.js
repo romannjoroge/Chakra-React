26602c99cebb90fa0766e7ad9e0aae007be9cea9
romannjoroge <roman.njoroge@njuguna.com>
2023-04-01 22:34:15 +0300

    feat: Adding Text component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { Button } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
 
 export default function App() {
   return (
     <ChakraProvider>
       <div className="app">
+        <Text as='section' fontSize='4xl' color='red' noOfLines={2}>
+          This is a paragraph that is styled using the Text's component props. The as prop changes what tag the component will be rendered to, 
+          the color prop changes the color, fontSize changes fontsize etc. It's possible for these props to hold values that are not valid CSS 
+          values.
+        </Text>
         <Button>This is a button</Button>
       </div>
     </ChakraProvider>
